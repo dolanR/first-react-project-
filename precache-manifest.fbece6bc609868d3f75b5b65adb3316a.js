b9853a7008b9d624fa663a4cb7e0045c95a809ea
Dolan Reynolds <reynoldsdolan@gmail.com>
2023-08-21 19:56:18 -0400

    Updates

---
+++ precache-manifest.fbece6bc609868d3f75b5b65adb3316a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "afe50811da19e89c62c1",
+    "url": "/first-react-project-/static/css/main.de80e359.chunk.css"
+  },
+  {
+    "revision": "afe50811da19e89c62c1",
+    "url": "/first-react-project-/static/js/main.5f4db6b5.chunk.js"
+  },
+  {
+    "revision": "8fd7b64da2d484d80be7",
+    "url": "/first-react-project-/static/js/runtime~main.e8886005.js"
+  },
+  {
+    "revision": "df9c9432ba417b62ce10",
+    "url": "/first-react-project-/static/js/2.04e5c296.chunk.js"
+  },
+  {
+    "revision": "cecf616dbfc830b280dc",
+    "url": "/first-react-project-/static/js/3.d7f7bd74.chunk.js"
+  },
+  {
+    "revision": "0acc157c1d69426ed20fee6422b8abf9",
+    "url": "/first-react-project-/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.05cb23748a66fc7927a83d22cfd15d86.js"
+  "/first-react-project-/precache-manifest.fbece6bc609868d3f75b5b65adb3316a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-gh-pages/index.html", {
+workbox.routing.registerNavigationRoute("/first-react-project-/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
